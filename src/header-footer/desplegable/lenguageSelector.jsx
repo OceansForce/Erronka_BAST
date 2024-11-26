ddaaa986bb4e77b2692a5100a770be1a2ae5a5ae
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-11-26 10:31:57 +0100

    add: hizkuntza aukeratu
    Aukeratu al da zein hizkuntzarekin nahi den web gunea

---
+++ src/header-footer/desplegable/lenguageSelector.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+
+const LanguageSelector = ({ changeLanguage }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Alterna la visibilidad del menú
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Cierra el menú si se hace clic fuera de él
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.closest('.language-selector') === null) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, []);
+
+  return (
+    <div className="relative language-selector">
+      <img
+        src="/img/icons/menu/language.svg"
+        className="w-10 h-10 cursor-pointer"
+        alt="Seleccionar idioma"
+        onClick={toggleDropdown}
+      />
+
+      {isOpen && (
+        <div className="absolute left-0 mt-2 w-40 bg-white border border-gray-300 rounded-lg shadow-md z-10">
+          <ul className="list-none p-2">
+            <li
+              className="cursor-pointer hover:bg-gray-100 p-2 rounded"
+              onClick={() => changeLanguage('eu')}  // Llama a changeLanguage con 'eu' para Euskera
+            >
+              Euskera
+            </li>
+            <li
+              className="cursor-pointer hover:bg-gray-100 p-2 rounded"
+              onClick={() => changeLanguage('es')}  // Llama a changeLanguage con 'es' para Español
+            >
+              Español
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LanguageSelector;
--- src/header-footer/header.js
+++ src/header-footer/header.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import i18n from './118n/i18n';
 import { useTranslation } from 'react-i18next';
+import LanguageSelector from './desplegable/lenguageSelector';  // Importamos el componente LanguageSelector
 
 const Header = () => {
   const { t, i18n } = useTranslation();
 
+  // Función para cambiar el idioma
   const changeLanguage = (lang) => {
     i18n.changeLanguage(lang);  // Cambia el idioma
   };
 
   return (
     <div className='flex space-x-4 items-center'>
-      <button onClick={() => changeLanguage('es')}>Español</button>
-      <button onClick={() => changeLanguage('eu')}>Euskera</button>
+      <img src='/img/logo.png' className='size-32' alt="Logo" />
       
-      <img src='/img/logo.png' className='size-32'/>
-      <div className=''>
+      <div>
         <ul className='list-none flex space-x-4'>
-            <li>{t('menu:hasiera')}</li>
-            <li>{t('menu:galduta')}</li>
-            <li>{t('menu:mapa')}</li>
-            <li>{t('menu:adopzio')}</li>
-            <li>{t('menu:denda')}</li>
+          <li><a href='#'>{t('menu:hasiera')}</a></li>
+          <li><a href='#'>{t('menu:galduta')}</a></li>
+          <li><a href='#'>{t('menu:mapa')}</a></li>
+          <li><a href='#'>{t('menu:adopzio')}</a></li>
+          <li><a href='#'>{t('menu:denda')}</a></li>
         </ul>
       </div>
+
+      {/* Componente LanguageSelector con la función changeLanguage como prop */}
+      <LanguageSelector changeLanguage={changeLanguage} />
     </div>
   );
 };

f12595ede8609228ccc19c330d2778f0d47def5c
OceansForce <julengarcia165@gmail.com>
2024-11-26 13:55:27 +0100

    Body-ak egiten

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import Header from './header-footer/header';
+import Body from './body/body.js';
 import './index.css';
+import './body/css.css';
 
 
 function App() {
   return (
-    <div className="App container">
-      <Header />
-    </div>
+    <>
+      <div className="App container">
+        <Header /> 
+      </div>
+      <div  className="App container justify-center items-center">
+        <Body />
+      </div>
+    </>
+    
   );
 }
 export default App;
--- src/body/body.js
+++ src/body/body.js
@@ -1,5 +1,45 @@
 function Body() {
-    return ;
+    return <div className="container items-center flex flex-col">
+            <div className="container justify-evenly items-center flex flex-row">
+                <div className="container flex flex-col justify-cemter">
+                    <div className="container">
+                        <img src="./img/¿sabias-que-la-nariz-de-cada-perro-es-unica.png" className="w-1/4 center"/>
+                    </div>
+                    <div>
+                        <p className="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis modi, 
+                            veritatis quas, distinctio mollitia iusto officiis quae consequatur totam ullam 
+                            aliquid placeat nam culpa ipsa? Pariatur nemo quasi expedita?</p>
+                    </div>
+                </div>
+                <div className="container flex flex-col justify-center">
+                    
+                    <img src="./img/logo.png" className="w-2/2 center"/>
+                    
+                    <div>
+                        <p className="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis modi, 
+                            veritatis quas, distinctio mollitia iusto officiis quae consequatur totam ullam 
+                            aliquid placeat nam culpa ipsa? Pariatur nemo quasi expedita?</p>
+                    </div>
+                </div>
+                <div className="container flex flex-col justify-center">
+                    <div>
+                        <img src="./img/logo.png" className="w-1/4 center"/>
+                    </div>
+                    <div>
+                        <p className="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis modi, 
+                            veritatis quas, distinctio mollitia iusto officiis quae consequatur totam ullam 
+                            aliquid placeat nam culpa ipsa? Pariatur nemo quasi expedita?</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h4>Gehiago</h4>
+                </div>
+            </div>
+            
+        </div>
+    ;
 }
 
 export default Body;

b7e5ec5e060ebf86490c509d44bef5bba440a6e6
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-11-26 13:41:50 +0100

    fix:aldatuta irudiak

--- src/header-footer/header.js
+++ src/header-footer/header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import i18n from './118n/i18n';
 import { useTranslation } from 'react-i18next';
-import LanguageSelector from './desplegable/lenguageSelector';  // Importamos el componente LanguageSelector
+import LanguageSelector from './desplegable/lenguageSelector';
+import DarkModeToggle from './dark-light/dark'
 
 const Header = () => {
   const { t, i18n } = useTranslation();
@@ -15,18 +16,20 @@ const Header = () => {
     <div className='flex space-x-4 items-center'>
       <img src='/img/logo.png' className='size-32' alt="Logo" />
       
-      <div>
+      <div className='justify-center'>
         <ul className='list-none flex space-x-4'>
-          <li><a href='#'>{t('menu:hasiera')}</a></li>
-          <li><a href='#'>{t('menu:galduta')}</a></li>
-          <li><a href='#'>{t('menu:mapa')}</a></li>
-          <li><a href='#'>{t('menu:adopzio')}</a></li>
-          <li><a href='#'>{t('menu:denda')}</a></li>
+          <li><a href="#" className="text-black dark:text-white">{t('menu:hasiera')}</a></li>
+          <li><a href='#' className="text-black dark:text-white">{t('menu:galduta')}</a></li>
+          <li><a href='#' className="text-black dark:text-white">{t('menu:mapa')}</a></li>
+          <li><a href='#' className="text-black dark:text-white">{t('menu:adopzio')}</a></li>
+          <li><a href='#' className="text-black dark:text-white">{t('menu:denda')}</a></li>
         </ul>
       </div>
 
       {/* Componente LanguageSelector con la función changeLanguage como prop */}
       <LanguageSelector changeLanguage={changeLanguage} />
+      <DarkModeToggle />
+      
     </div>
   );
 };

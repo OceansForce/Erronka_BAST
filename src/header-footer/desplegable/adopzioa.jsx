48965b763ccc93ae082c5bd12a2678c8502a4ce3
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-11-27 10:01:49 +0100

    fix: adopzio botoia funtzionatzen du

--- src/header-footer/desplegable/adopzioa.jsx
+++ src/header-footer/desplegable/adopzioa.jsx
@@ -1,62 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const UserOptions = () => {
+const Adopzioa = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { t } = useTranslation();
-  // Alterna la visibilidad del menú
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
+
+  // Alterna la visibilidad del menú cuando el ratón entra o sale
+  const handleMouseEnter = () => {
+    setIsOpen(true);
   };
 
-  // Cierra el menú si se hace clic fuera de él
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (event.target.closest('.language-selector') === null) {
-        setIsOpen(false);
-      }
-    };
-
-    document.addEventListener('click', handleClickOutside);
-    return () => document.removeEventListener('click', handleClickOutside);
-  }, []);
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <div className="relative language-selector">
-      <img
-        src="/img/icons/users/user-dog.jpg"
-        className="w-10 h-10 cursor-pointer rounded-full"
-        alt="Seleccionar idioma"
-        onClick={toggleDropdown}
-      />
+    <div 
+      className="relative language-selector"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <li>
+        <a href="#" className="text-black dark:text-white">
+          {t('menu:adopzio')}
+        </a>
+      </li>
 
       {isOpen && (
-        <div className="absolute left-0 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-md z-10">
+        <div className="absolute left-0 pt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-md z-10">
           <ul className="list-none p-2">
-
-
             <li className="cursor-pointer hover:bg-gray-100 p-2 rounded">
-              <a href='#' className='flex items-center'>
-                <img className='size-8 rounded-full' src='/img/icons/users/user-dog.jpg'/>
-                <h4 className='ml-3'>{t('adopzio:txakurra')}</h4>
+              <a href="#" className="flex items-center">
+                <img className="size-8 rounded-full" src="/img/icons/users/user-dog.jpg" />
+                <h4 className="ml-3">{t('adopzio:txakurra')}</h4>
               </a>
             </li>
             <li className="cursor-pointer hover:bg-gray-100 p-2 rounded">
-              <a href='#' className='flex items-center'>
-                <img className='size-8 rounded-full' src='/img/icons/users/chat.svg'/>
-                <h4 className='ml-3'>{t('adopzio:ppp')}</h4>
+              <a href="#" className="flex items-center">
+                <img className="size-8 rounded-full" src="/img/icons/users/chat.svg" />
+                <h4 className="ml-3">{t('adopzio:ppp')}</h4>
               </a>
             </li>
             <li className="cursor-pointer hover:bg-gray-100 p-2 rounded">
-              <a href='#' className='flex items-center'>
-                <img className='size-8 rounded-full' src='/img/icons/users/config.svg'/>
-                <h4 className='ml-3'>{t('adopzio:katua')}</h4>
+              <a href="#" className="flex items-center">
+                <img className="size-8 rounded-full" src="/img/icons/users/config.svg" />
+                <h4 className="ml-3">{t('adopzio:katua')}</h4>
               </a>
             </li>
             <li className="cursor-pointer hover:bg-gray-100 p-2 rounded">
-              <a href='#' className='flex items-center'>
-                <img className='size-8 rounded-full' src='/img/icons/users/exit.svg'/>
-                <h4 className='ml-3'>{t('adopzio:Besteak')}</h4>
+              <a href="#" className="flex items-center">
+                <img className="size-8 rounded-full" src="/img/icons/users/exit.svg" />
+                <h4 className="ml-3">{t('adopzio:Besteak')}</h4>
               </a>
             </li>
           </ul>
@@ -66,4 +60,4 @@ const UserOptions = () => {
   );
 };
 
-export default UserOptions;
+export default Adopzioa;

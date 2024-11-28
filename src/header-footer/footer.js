ef7edd280ef1e9810ed2a68ec92f0b892a9e6335
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-11-28 10:54:32 +0100

    add: Aukeratu daiteke contac eta datuen artean

--- src/header-footer/footer.js
+++ src/header-footer/footer.js
@@ -1,38 +1,35 @@
-import React from 'react';
-import i18n from '../118n/menu';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Profile from './footer/profile-contact/profile';
 import Datuak from './footer/datuak';
 import Formulario from './footer/formulario';
 
-
-
 const Footer = () => {
   const { t, i18n } = useTranslation();
 
+  const [activeSection, setActiveSection] = useState('Profile');
 
   return (
-    <div className='space-x-4 w-full p-16 bg-primary dark:bg-dark md:flex-row'>
-
-        <Datuak />
-        <Formulario />
-
-      <div className='flex flex-row justify-between mt-16 text-neutral-700 dark:text-gray-300'>
-        <div className='w-1/4 text-center'>
-          <p>Typeface by Manex and Julen</p>
-          <div className='space-x-4'>
-            <a href='#'>© 2024</a>
-            <a href='#'>Data Privacy</a>
-            <a href='#'>Imprint</a>
-          </div>
-          
-        </div>
-        <div className='w-1/4 '>
-          <Profile />
-        </div>
-        <div className='w-1/4'></div>
-      </div>
+    <div className="space-x-4 w-full p-16 bg-primary dark:bg-dark md:flex-row">
       
+      {activeSection === 'Profile' && <Datuak />}
+      {activeSection === 'Contact' && <Formulario />}
+
+      <div className="flex flex-row justify-between mt-16 text-neutral-700 dark:text-gray-300">
+        <div className="w-1/4 text-center">
+          <p>Typeface by Manex and Julen</p>
+          <div className="space-x-4">
+            <a href="#">© 2024</a>
+            <a href="#">Data Privacy</a>
+            <a href="#">Imprint</a>
+          </div>
+        </div>
+       
+        <div className="w-1/4">
+          <Profile onActiveChange={setActiveSection} />
+        </div>
+        <div className="w-1/4"></div>
+      </div>
     </div>
   );
 };

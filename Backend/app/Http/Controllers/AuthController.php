e5b3ec488b5a6259fea1a848d5b20370be5e803d
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-12-11 10:39:08 +0000

    Errore mezua bueltatu dezake

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
+use Carbon\Carbon;
+
 class AuthController extends Controller
 {
     public function login(Request $request)
@@ -19,13 +21,13 @@ class AuthController extends Controller
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['Las credenciales proporcionadas son incorrectas.'],
-            ]);
+            return response()->json([
+                'error' => 'Las credenciales proporcionadas son incorrectas.',
+            ], 401);
         }
 
         // Generar el token de acceso
-        $token = $user->createToken('YourAppName')->plainTextToken;
+        $token = $user->createToken('YourAppName', ['*'], Carbon::now()->addDays(5))->plainTextToken;
 
         return response()->json([
             'token' => $token

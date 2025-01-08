3f92c152008878999537c127d876efae92e59457
Manex <maranzadieg23wg@ikzubirimanteo.com>
2025-01-08 09:38:16 +0100

    desesperacion

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -21,7 +21,7 @@ Route::post('login', [AuthController::class, 'login']);
 use App\Http\Controllers\NewsController;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('news', [NewsController::class, 'store'])->middleware('NewsCreate');
+    Route::post('news', [NewsController::class, 'store'])->middleware('\App\Http\Middleware\CheckCreateNewsPermissions');
     Route::put('news/{news}', [NewsController::class, 'update']);
     Route::delete('news/{news}', [NewsController::class, 'destroy'])->middleware('checkProtektora');
 });

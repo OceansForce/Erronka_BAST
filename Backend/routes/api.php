da59c2dbe278e5597c34db82a0b8ec5e634d2d9a
Manex <maranzadieg23wg@ikzubirimanteo.com>
2025-01-10 11:10:54 +0100

    add: apian gehituta

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -33,6 +33,8 @@ Route::get('user-data', [UserController::class, 'getUser'])->middleware('\App\Ht
 // ERABILTZAILEAREN DATUAK LORTU + BERE ANIMALIAK
 Route::put('user-data-edit', [UserCreateController::class, 'edit'])->middleware('\App\Http\Middleware\BasicUserAuth');  // Ruta para crear un usuario
 
+// ERABILTZAILE bateri ezarri protektora bat
+Route::put('user-add-protectora', [UserCreateController::class, 'addProtectora'])->middleware('\App\Http\Middleware\CheckCreateNewsPermissions');  // Ruta para crear un usuario
 
 // SAIOA HASI
 use App\Http\Controllers\AuthController;

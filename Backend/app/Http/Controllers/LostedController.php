7ab2e4fc75acab2419bc1ad7419b3aba62b7b5bc
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2025-01-30 18:41:20 +0100

    fix: galduta izena aldatuta
    Aldatu egin zaio galduta modeloari, losted-en ordez

--- Backend/app/Http/Controllers/LostedController.php
+++ Backend/app/Http/Controllers/LostedController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Animals;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use App\Models\Losted;
 
 
 class LostedController extends Controller

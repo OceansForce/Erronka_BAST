1f98c0d4f517e0678dab42435971712499163406
Manex Aranzadi <manexae@protonmail.com>
2025-02-06 14:17:44 +0100

    add: cancel email
    Ezestatzen bada adopzioa, mezu bat bidaltzen da erabiltzaileari esanez

--- Backend/app/Http/Controllers/AdoptController.php
+++ Backend/app/Http/Controllers/AdoptController.php
@@ -113,7 +113,7 @@ class AdoptController extends Controller
             'adoptToken' => null,
         ]);
 
-        Mail::send('emails.adopted', ['token' => $verificationToken, 'user' => $user, 'animal'=> $animal], function ($message) use ($user) {
+        Mail::send('emails.cancel', ['token' => $verificationToken, 'user' => $user, 'animal'=> $animal], function ($message) use ($user) {
             $message->to($protectora->email);
             $message->subject("Lamentamos que no hayas podido adoptar a {$animal->name}");
         });
---
+++ Backend/resources/views/emails/cancel.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Lamentamos que no hayas podido adoptar a {{ $animal->name }}</title>
+</head>
+<body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+    <h1 style="color: #cc0000;">Lamentamos que no hayas podido adoptar a {{ $animal->name }}</h1>
+    
+    <p>Hola {{ $user->name }},</p>
+    
+    <p>
+        Nos entristece comunicarte que, lamentablemente, no se ha podido concretar la adopción de <strong>{{ $animal->name }}</strong>.
+    </p>
+    
+    <p>
+        Sabemos lo importante que es para ti brindar un hogar a un animal necesitado, y nos hubiera encantado contar contigo como parte de la familia de {{ $animal->name }}.
+    </p>
+    
+    <p>
+        Si tienes alguna pregunta o necesitas más información sobre el proceso o futuras oportunidades, por favor no dudes en ponerte en contacto con nosotros.
+    </p>
+    
+    <p>
+        Agradecemos sinceramente tu interés y esperamos poder contar contigo en futuras oportunidades.
+    </p>
+    
+    <p>Un cordial saludo,</p>
+    <p>El equipo de Adopciones</p>
+</body>
+</html>

f65040d1afd4a58b527eb14c79f1fbe31c6fb0ab
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2025-02-06 18:30:44 +0100

    add: protektorak lortu eta sortu al dira

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -94,6 +94,14 @@ Route::get('/verify-adoption/{token}', [AdoptController::class, 'adoptConfirmati
 Route::get('/adop-cancel/{token}', [AdoptController::class, 'adoptCancel']);
 
 
+
+// Protectora
+use App\Http\Controllers\ProtektoraController;
+Route::post('/protektora-create', [ProtektoraController::class, 'createProtektora'])->middleware('auth:sanctum');
+Route::get('/all-protektora', [ProtektoraController::class, 'getAllProtektoras']);
+
+
+
 // UP images
 /*use App\Http\Controllers\ImageController;
 Route::post('/upload-image', [ImageController::class, 'upload'])->middleware('auth:sanctum');

14e70a2dd36584711073a7797def6d338ea92ce9
Manex <maranzadieg23wg@ikzubirimanteo.com>
2025-02-07 10:57:58 +0100

    tabulation problem

--- Frontend/src/pages/Edit_animal.js
+++ Frontend/src/pages/Edit_animal.js
@@ -131,110 +131,110 @@ function Edit_animal() {
     return (
         <> 
             <div className='container flex justify-center erdian'>
-            <div className='flex flex-col dark:bg-dark bg-primary p-6 m-10 w-full rounded-lg text-center border-black dark:border-transparent border-2'>
-              <div className='w-full flex'>
-                <BackButton targetPage={ruta} width={20}/>
+                <div className='flex flex-col dark:bg-dark bg-primary p-6 m-10 w-full rounded-lg text-center border-black dark:border-transparent border-2'>
+                    <div className='w-full flex'>
+                        <BackButton targetPage={ruta} width={20}/>
 
-                <div className='w-11/12 flex flex-row space-x-4 justify-end'>
-                    <LanguageSelector className='w-1/2' changeLanguage={changeLanguage} />
-                    <DarkModeToggle className='w-1/2' />
-                </div>
-              </div>
+                        <div className='w-11/12 flex flex-row space-x-4 justify-end'>
+                            <LanguageSelector className='w-1/2' changeLanguage={changeLanguage} />
+                            <DarkModeToggle className='w-1/2' />
+                        </div>
+                    </div>
 
-              <p className='font-semibold text-2xl my-5 dark:text-white uppercase'>{t('ad_galduta:animalEdit')}</p>
-              <form className='flex flex-col text-left' onSubmit={handleSubmit}>
+                    <p className='font-semibold text-2xl my-5 dark:text-white uppercase'>{t('ad_galduta:animalEdit')}</p>
+                    <form className='flex flex-col text-left' onSubmit={handleSubmit}>
 
-                <div className='flex flex-row'>
-                    <div className='flex flex-col w-1/2 mr-10'>
-                        <label className='font-semibold dark:text-white'>{t('ad_galduta:Izena')}</label>
-                        <input className='mb-2 dark:border-primary rounded-lg ' type='text' name='name'  value={formData.name} onChange={handleChange} required/>
+                        <div className='flex flex-row'>
+                            <div className='flex flex-col w-1/2 mr-10'>
+                                <label className='font-semibold dark:text-white'>{t('ad_galduta:Izena')}</label>
+                                <input className='mb-2 dark:border-primary rounded-lg ' type='text' name='name'  value={formData.name} onChange={handleChange} required/>
+                                
+                                <label className='font-semibold dark:text-white'>{t('ad_galduta:Profil')}</label>
+                                <input 
+                                    className='mb-2 dark:border-primary rounded-lg dark:text-white' 
+                                    type='file'
+                                    name='img'
+                                    //value={formData.img}
+                                    onChange={(e) => {
+                                        const file = e.target.files[0];
+                                        console.log("File selected:", file); // Verificar si el archivo está seleccionado
+                                        setFormData({ ...formData, img: file });
+                                    }}
+                                    />
+                            </div>
+                            <div className='flex flex-col w-1/2'>
+                                <div className=' w-1/2'>
+                                    <label className='mt-2 font-semibold dark:text-white'>{t("ad_galduta:Esterilizatua")}</label> 
+                                    <div className='ml-3'>
+                                    <input
+                                        type='radio'
+                                        name='Esterilizatua'
+                                        id='esterilizatua1'
+                                        value={1}
+                                        onChange={(e) => setEsterilizado(parseInt(e.target.value))}
+                                        checked={esterilizado === 1}
+                                    />
+                                    <label className='ml-1 dark:text-white fonts_ubutu'>{t('ad_galduta:Bai')}</label>
+                                </div>
+
+                                <div className='ml-3 mt-2'>
+                                    <input
+                                        type='radio'
+                                        name='Esterilizatua'
+                                        id='esterilizatua2'
+                                        value={2}
+                                        onChange={(e) => setEsterilizado(parseInt(e.target.value))}
+                                        checked={esterilizado === 2}
+                                    />
+                                    <label className='ml-1 dark:text-white fonts_ubutu'>{t('ad_galduta:Ez')}</label>
+                                </div>
+
+                                </div>
+                                <label className='font-semibold dark:text-white'>{t('Ad_adoptatu:Etxekoa')}</label>
+                                <div className='ml-3'>
+                                    <input
+                                        type='radio'
+                                        name='Etxekoa'
+                                        id='etxekoa1'
+                                        value={1}
+                                        onChange={(e) => setEtxekoa(parseInt(e.target.value))}
+                                        checked={etxekoa === 1} // Si etxekoAnimalia es 1, el primer botón se selecciona
+                                        required
+                                    />
+                                    <label className='ml-1 dark:text-white fonts_ubutu'>{t('ad_galduta:Bai')}</label>
+                                </div>
+
+                                <div className='ml-3'>
+                                    <input
+                                        type='radio'
+                                        name='Etxekoa'
+                                        id='etxekoa2'
+                                        value={0}
+                                        onChange={(e) => setEtxekoa(parseInt(e.target.value))}
+                                        checked={etxekoa === 0} // Si etxekoAnimalia es 2, el segundo botón se selecciona
+                                        required
+                                    />
+                                    <label className='ml-1 dark:text-white fonts_ubutu'>{t('ad_galduta:Ez')}</label>
+                                </div>
+
+                            </div>
                         
-                        <label className='font-semibold dark:text-white'>{t('ad_galduta:Profil')}</label>
-                        <input 
-                            className='mb-2 dark:border-primary rounded-lg dark:text-white' 
-                            type='file'
-                            name='img'
-                            //value={formData.img}
-                            onChange={(e) => {
-                                const file = e.target.files[0];
-                                console.log("File selected:", file); // Verificar si el archivo está seleccionado
-                                setFormData({ ...formData, img: file });
-                            }}
-                            />
-                    </div>
-                    <div className='flex flex-col w-1/2'>
-                        <div className=' w-1/2'>
-                            <label className='mt-2 font-semibold dark:text-white'>{t("ad_galduta:Esterilizatua")}</label> 
-                            <div className='ml-3'>
-                            <input
-                                type='radio'
-                                name='Esterilizatua'
-                                id='esterilizatua1'
-                                value={1}
-                                onChange={(e) => setEsterilizado(parseInt(e.target.value))}
-                                checked={esterilizado === 1}
-                            />
-                            <label className='ml-1 dark:text-white fonts_ubutu'>{t('ad_galduta:Bai')}</label>
                         </div>
-
-                        <div className='ml-3 mt-2'>
-                            <input
-                                type='radio'
-                                name='Esterilizatua'
-                                id='esterilizatua2'
-                                value={2}
-                                onChange={(e) => setEsterilizado(parseInt(e.target.value))}
-                                checked={esterilizado === 2}
-                            />
-                            <label className='ml-1 dark:text-white fonts_ubutu'>{t('ad_galduta:Ez')}</label>
-                        </div>
-
-                        </div>
-                        <label className='font-semibold dark:text-white'>{t('Ad_adoptatu:Etxekoa')}</label>
-                        <div className='ml-3'>
-                            <input
-                                type='radio'
-                                name='Etxekoa'
-                                id='etxekoa1'
-                                value={1}
-                                onChange={(e) => setEtxekoa(parseInt(e.target.value))}
-                                checked={etxekoa === 1} // Si etxekoAnimalia es 1, el primer botón se selecciona
-                                required
-                            />
-                            <label className='ml-1 dark:text-white fonts_ubutu'>{t('ad_galduta:Bai')}</label>
-                        </div>
-
-                        <div className='ml-3'>
-                            <input
-                                type='radio'
-                                name='Etxekoa'
-                                id='etxekoa2'
-                                value={0}
-                                onChange={(e) => setEtxekoa(parseInt(e.target.value))}
-                                checked={etxekoa === 0} // Si etxekoAnimalia es 2, el segundo botón se selecciona
-                                required
-                            />
-                            <label className='ml-1 dark:text-white fonts_ubutu'>{t('ad_galduta:Ez')}</label>
-                        </div>
-
-                    </div>
-                  
-                </div>
+                            
                     
-              
-                <div className='flex flex-row mt-2'>
-                  <div className=' w-full flex flex-col'>
-                    <label className='font-semibold dark:text-white'>{t('ad_galduta:Deskribapena')}</label>
-                    <textarea className='dark:border-primary border-black border-2 rounded-lg' rows={6} name='descripcion' value={formData.descripcion} onChange={handleChange} required></textarea>
-                  </div>
-                </div>
-            
-                <SendButom value={t('saioa_sortu:input')} />
-            </form>
+                        <div className='flex flex-row mt-2'>
+                        <div className=' w-full flex flex-col'>
+                            <label className='font-semibold dark:text-white'>{t('ad_galduta:Deskribapena')}</label>
+                            <textarea className='dark:border-primary border-black border-2 rounded-lg' rows={6} name='descripcion' value={formData.descripcion} onChange={handleChange} required></textarea>
+                        </div>
+                        </div>
+                    
+                        <SendButom value={t('saioa_sortu:input')} />
+                    </form>
 
-          </div>
-        </div>
-      </>
+                </div>
+            </div>
+        </>
     );
   }
   
